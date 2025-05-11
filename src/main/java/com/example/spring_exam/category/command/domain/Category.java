0901364411541c9609c0006d50b26558f31c7de9
beetrbgus <devbeetrb0214@gmail.com>
2025-05-11 21:54:56 +0900

    feat - 카테고리 도메인 추가

---
+++ src/main/java/com/example/spring_exam/category/command/domain/Category.java
@@ -0,0 +1,62 @@
+package com.example.spring_exam.category.command.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreRemove;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Integer depth;
+    private Integer sortOrder;
+    private Boolean isVisible;
+    private Boolean isLeaf;
+    private String slug; // web에서 나타내기 좋은 공백없는 URL 친화적 이름 (예: pasta-sauce)
+    private String description;
+    private String thumbnailUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> children = new ArrayList<>();
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    @PreRemove
+    public void updateLeafStatus() {
+        this.isLeaf = children.isEmpty();
+    }
+}
