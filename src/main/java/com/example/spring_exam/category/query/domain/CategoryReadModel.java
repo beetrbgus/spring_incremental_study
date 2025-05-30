2681349916fbb38eb3b00424a3a9931c9ec47f88
beetrbgus <devbeetrb0214@gmail.com>
2025-05-31 01:43:54 +0900

    feature - 카테고리 전체 조회 API 추가

---
+++ src/main/java/com/example/spring_exam/category/query/domain/CategoryReadModel.java
@@ -0,0 +1,46 @@
+package com.example.spring_exam.category.query.domain;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CategoryReadModel {
+    private Long id;
+    private String name;
+    private Integer depth;
+    private Integer sortOrder;
+    private Boolean isVisible;
+    private Boolean isLeaf;
+    private String slug;
+    private String description;
+
+    private Long parentId;
+
+    private List<CategoryReadModel> children = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @QueryProjection
+    public CategoryReadModel(Long id, String name, Integer depth, Integer sortOrder, Boolean isVisible, Boolean isLeaf, String slug, String description, Long parentId, List<CategoryReadModel> children, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.depth = depth;
+        this.sortOrder = sortOrder;
+        this.isVisible = isVisible;
+        this.isLeaf = isLeaf;
+        this.slug = slug;
+        this.description = description;
+        this.parentId = parentId;
+        this.children = children;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
